08fb0afdcbcdcbf5e71eb6a47d1e0606a28e9838
Suhaibhus <159073300+Suhaibhus@users.noreply.github.com>
2024-12-22 23:13:31 -0600

    Delete Email-Summarizer-Project/Frontend directory

--- Email-Summarizer-Project/Frontend/background.js
+++
@@ -1,8 +0,0 @@
-//This is to make sure that this extension can run on any site. 
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("mail.google.com/mail")) {
-        chrome.tabs.sendMessage(tabId, {
-            type: "Email_page",
-        });
-    }
-});
